adb7f1d63c3b65520e22d5f569f7e91b89fe552e
Connor Young <>
2022-01-18 01:08:16 +0800

    Include new modules in lib.rs

--- parrot/src/lib.rs
+++ parrot/src/lib.rs
@@ -1,8 +1,14 @@
-#[warn(clippy::pedantic)]
+#[deny(clippy::pedantic)]
+#[warn(dead_code)]
 
 pub mod painter;
+pub mod pipeline;
+pub mod binding;
+pub mod shader;
 pub mod device;
 pub mod transform;
 pub mod vertex;
 pub mod buffers;
 pub mod texture;
+pub mod sampler;
+pub mod colour;
